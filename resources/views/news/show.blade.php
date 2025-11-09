f258af9630ad3991c8b5d88ad09282022916ca09
Rafi Ramadhan <rafiramadhanarr17@gmail.com>
2025-11-09 20:05:42 +0700

    fix text justify

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -55,7 +55,7 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-12 col-md-8">
-                    <div class="single-post-text">
+                    <div class="single-post-text text-justify">
                         {!! $article->deskripsi !!}
                     </div>
                 </div>

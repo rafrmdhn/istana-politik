96ea59bb1cd1609f31dd2a7fecc4faf894cc2883
Dhimasdar Agdeli R.A <dhimasdar.agdeli@bni.co.id>
2025-11-11 11:42:31 +0700

    fix numbering and bulleting

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Core Style CSS -->
     <link rel="stylesheet" href="{{ asset('css/core-style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/core-style.patched.css') }}">
     <link rel="stylesheet" href="{{ asset('style.css') }}">
 
     <!-- Responsive CSS -->
